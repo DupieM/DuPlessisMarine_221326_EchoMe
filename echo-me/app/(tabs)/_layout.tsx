6f4510699aaaed344b8b8ab8b0d9349125c21b72
DupieM <221326@virtualwindow.co.za>
2025-06-04 12:44:33 +0200

    Fixing navigation bar

--- echo-me/app/(tabs)/_layout.tsx
+++ echo-me/app/(tabs)/_layout.tsx
@@ -1,17 +1,22 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
-
+import { Image, Platform } from 'react-native';
 import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
-import HomeSvg from '../../assets/images/home.svg';
-import JourneySvg from '../../assets/images/journey.svg'; 
-import ChatSvg from '../../assets/images/chat.svg';      
-import HobbiesSvg from '../../assets/images/Mask group.svg'; 
-import ShowcaseSvg from '../../assets/images/projects.svg'; 
+
+import HomeActive from '../../assets/images/Navbar/Home_Active.png';
+import HomeInactive from '../../assets/images/Navbar/Home_Inactive.png';
+import JourneyActive from '../../assets/images/Navbar/Journey_Active.png';
+import JourneyInactive from '../../assets/images/Navbar/Journey_Inactive.png';
+import ChatActive from '../../assets/images/Navbar/Chat_Active.png';
+import ChatInactive from '../../assets/images/Navbar/Chat_Inactive.png';
+import HobbiesActive from '../../assets/images/Navbar/Hobbies_Active.png';
+import HobbiesInactive from '../../assets/images/Navbar/Home_Inactive.png';
+import ShowcaseActive from '../../assets/images/Navbar/Showcase_Active.png';
+import ShowcaseInactive from '../../assets/images/Navbar/Showcase_Inactive.png';
 
 
 export default function TabLayout() {
@@ -48,35 +53,66 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={focused ? HomeActive : HomeInactive}
+              style={{ width: 24, height: 24 }}
+              resizeMode="contain"
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="journey"
         options={{
           title: 'Journey',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={focused ? JourneyActive : JourneyInactive}
+              style={{ width: 24, height: 24 }}
+              resizeMode="contain"
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="chat"
         options={{
           title: 'Chat',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={focused ? ChatActive : ChatInactive}
+              style={{ width: 24, height: 24 }}
+              resizeMode="contain"
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="hobbies"
         options={{
           title: 'Hobbies',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={focused ? HobbiesActive : HobbiesInactive}
+              style={{ width: 24, height: 24 }}
+              resizeMode="contain"
+            />
+          ),
         }}
       />
+
       <Tabs.Screen
         name="showcase"
         options={{
           title: 'Showcase',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={focused ? ShowcaseActive : ShowcaseInactive}
+              style={{ width: 24, height: 24 }}
+              resizeMode="contain"
+            />
+          ),
         }}
       />
     </Tabs>
